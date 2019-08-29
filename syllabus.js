952ea02987b0b52c6a03d42eadc00129de4048d5
Renne Castro <rcastro2@schools.nyc.gov>
2019-08-29 01:38:21 -0400

    updates syllabus

--- syllabus.js
+++ syllabus.js
@@ -1,19 +1,20 @@
-var classes = [{date:"Jan 30",topic:"Course Introduction; Programming Concepts; Numerical representation of information"},
-                {date:"Feb 6",topic:"Introduction to environment; Output of information"},
-                {date:"Feb 13",topic:"Variables, Decisions, Enabling Intelligence (1/2)"},
-                {date:"Feb 20",topic:"Variables, Decisions, Enabling Intelligence (2/2)"},
-                {date:"Feb 27",topic:"Iteration of a process; Representing information as a collection (1/2)"},
-                {date:"Mar 6",topic:"Iteration of a process; Representing information as a collection (2/2)"},
-                {date:"Mar 13",topic:"Midterm"},
-                {date:"Mar 20",topic:"List of Information"},
-                {date:"Mar 27",topic:"Encapsulation of a process (1/2)"},
-                {date:"Apr 3",topic:"Encapsulation of a process (2/2)"},
-                {date:"Apr 10",topic:"Course Project - Team Formation / Idea Selection"},
-                {date:"Apr 17",topic:"Course Project - Implementation (Draft)"},
-                {date:"May 1",topic:"Course Project - Implementation (Prototype), Project Prep"},
-                {date:"May 8",topic:"Course Project - Presentation / Course Review"},
-                {date:"May 15",topic:"Final"}];
-var semester = "SP19";
+var classes = [{date:"Aug 28",topic:"Course Introduction; Programming Concepts; Numerical representation of information"},
+                {date:"Sep 4",topic:"Introduction to environment; Output of information"},
+                {date:"Sep 11",topic:"Variables, Decisions, Enabling Intelligence (1/2)"},
+                {date:"Sep 18",topic:"Variables, Decisions, Enabling Intelligence (2/2)"},
+                {date:"Sep 25",topic:"Iteration of a process; Representing information as a collection (1/2)"},
+                {date:"Oct 2",topic:"Iteration of a process; Representing information as a collection (2/2)"},
+                {date:"Oct 16",topic:"Midterm"},
+                {date:"Oct 23",topic:"List of Information"},
+                {date:"Oct 30",topic:"Encapsulation of a process (1/2)"},
+                {date:"Nov 6",topic:"Encapsulation of a process (2/2)"},
+                {date:"Nov 13",topic:"Course Project - Team Formation / Idea Selection"},
+                {date:"Nov 20",topic:"Course Project - Implementation (Draft)"},
+                {date:"Nov 27",topic:"Course Project - Implementation (Prototype), Project Prep"},
+                {date:"Dec 4",topic:"Course Project - Presentation"},
+                {date:"Dec 11",topic:"Single Board Computers / Course Review"},
+                {date:"Dec 18",topic:"Final"}];
+var semester = "FA19";
 					
 String.prototype.toCapitalize = function() {
     s = (this.length < 2) ? this.toUpperCase() : this.substr( 0, 1 ).toUpperCase() + this.substr( 1 ).toLowerCase();
